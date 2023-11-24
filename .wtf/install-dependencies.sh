29c1b4da22eb05943eeec33ef6f59fb9b2038824
Gilles Perreymond <gilles.perreymond@metronlab.com>
2023-11-24 12:36:08 +0100

    chore(): add consul to asdf

--- .wtf/install-dependencies.sh
+++ .wtf/install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 asdf plugin add nomad https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin add consul https://github.com/asdf-community/asdf-hashicorp.git
 asdf plugin add kubectl https://github.com/asdf-community/asdf-kubectl.git
 asdf plugin add helm https://github.com/Antiarchitect/asdf-helm.git
 asdf plugin add kind https://github.com/reegnz/asdf-kind.git
