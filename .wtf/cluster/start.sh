78de181b66d80cdfd7bad1f217db12a2a5250840
Gilles Perreymond <gilles.perreymond@metronlab.com>
2023-12-02 14:11:14 +0100

    feat(nomad): try consul inject

--- .wtf/cluster/start.sh
+++ .wtf/cluster/start.sh
@@ -5,7 +5,3 @@ k3d cluster start dev-local
 # change kubernetes context
 kubectx k3d-dev-local
 kubectl cluster-info --context k3d-dev-local
-
-kubectl taint nodes k3d-dev-local-server-0 key=node-master:NoSchedule
-kubectl taint nodes k3d-dev-local-server-1 key=node-master:NoSchedule
-kubectl taint nodes k3d-dev-local-server-2 key=node-master:NoSchedule
