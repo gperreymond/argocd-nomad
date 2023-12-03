54d136a4c7c9290598740c34f02efde736e9663d
Gilles Perreymond <gilles.perreymond@metronlab.com>
2023-12-03 15:44:20 +0100

    feat(nomad): prepare consul client done

--- .wtf/nomad/boostrap-server.sh
+++ .wtf/nomad/boostrap-server.sh
@@ -39,13 +39,13 @@ echo "[INFO] namespace.................. $namespace"
 echo "====================================================================="
 echo ""
 
+mkdir certs
+
 # -----------------------
 # GENERATE GOSSIP ENCRYPTION KEY
 # -----------------------
 
 secret_exists=$(kubectl get secret $secret_name -n $namespace 2>/dev/null)
-
-mkdir certs
 if [ -z "$secret_exists" ]; then
     echo "[INFO] secret $secret_name does not exist in namespace $namespace"
     uuid=$(curl -s https://www.uuidgenerator.net/api/version4)
@@ -56,25 +56,45 @@ else
 fi
 
 token=$(kubectl get secret $secret_name -n $namespace -o jsonpath='{.data.bootstrap\.token}' | base64 --decode)
-echo "[INFO] acl token is $token"
+echo "[INFO]... acl token is $token"
 echo $token > certs/bootstrap.token
+
+# -----------------------
+# DOWNLOAD NOMAD CERTS
+# -----------------------
+
+tls_secret_name="nomad-server-tls-certs"
+tls_secret_exists=$(kubectl get secret $tls_secret_name -n $namespace 2>/dev/null)
+if [ -z "$tls_secret_exists" ]; then
+    echo "[ERROR] secret $tls_secret_name does not exist in namespace $namespace"
+    exit 1
+else
+    echo "[INFO] secret $tls_secret_name exists in namespace $namespace"
+fi
+
+echo "[INFO] download certs"
+kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r '.data."nomad-agent-ca.pem"' | base64 --decode > certs/nomad-agent-ca.pem
+kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r --arg REGION "$region" '.data[$REGION + "-cli-nomad.pem"]' | base64 --decode > certs/$region-cli-nomad.pem
+kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r --arg REGION "$region" '.data[$REGION + "-cli-nomad-key.pem"]' | base64 --decode > certs/$region-cli-nomad-key.pem
 
 # -----------------------
 # VERIFY BOOTSTRAP NOMAD
 # -----------------------
 
+nomad server members \
+    -address=https://nomad.docker.localhost \
+    -region=$region \
+    -ca-cert=certs/nomad-agent-ca.pem \
+    -client-cert=certs/$region-cli-nomad.pem \
+    -client-key=certs/$region-cli-nomad-key.pem \
+    -json
+
+exit 0
+
 # -----------------------
 # RUN BOOTSTRAP NOMAD
 # -----------------------
 
-tls_secret_name="nomad-server-tls-certs"
-# retrieve keys from the secret
-keys=$(kubectl get secret $tls_secret_name -n $namespace -o jsonpath="{.data}" | jq -r 'keys[]')
-# loop through each key and save its value to a file
-echo "[INFO] download certs"
-kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r '.data."nomad-agent-ca.pem"' | base64 --decode > certs/nomad-agent-ca.pem
-kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r --arg REGION "$region" '.data[$REGION + "-cli-nomad.pem"]' | base64 --decode > certs/$region-cli-nomad.pem
-kubectl get secret nomad-server-tls-certs -n nomad-system -o json | jq -r --arg REGION "$region" '.data[$REGION + "-cli-nomad-key.pem"]' | base64 --decode > certs/$region-cli-nomad-key.pem
 nomad acl bootstrap \
     -address=https://nomad.docker.localhost \
     -region=$region \
